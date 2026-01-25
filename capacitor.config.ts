6d5afa2f2914b5d4d7170cb232eb2e7c2a2b8ec1
yNiklas <niklas@magic-computer.de>
2026-01-25 13:28:15 +0100

    Appid

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.medbox.app',
   appName: 'MedBoxApp',
   webDir: 'www',
   plugins: {
